7a170126fa56dc5691a535e66730af90d9e6aeef
Abdulwarith <adebayoayomide646@gmail.com>
2024-04-23 13:31:52 +0100

    presale page

--- src/assets/wrappers/Presale.js
+++ src/assets/wrappers/Presale.js
@@ -73,7 +73,7 @@ const Wrapper = styled.section`
             text-transform: uppercase;
           }
 
-          span:nth-child(2){
+          span:nth-child(2) {
             color: black;
           }
         }
@@ -147,6 +147,109 @@ const Wrapper = styled.section`
               line-height: 19.15px;
               letter-spacing: 3px;
               font-family: "MilkyNice", sans-serif;
+            }
+          }
+        }
+      }
+    }
+  }
+
+  @media (max-width: 992px) {
+    .header-container {
+      button {
+        padding: 3.5px 17.52px 5.26px 17.52px;
+        border: 1.31px solid black;
+        border-radius: 17.96px;
+        font-size: 12px;
+        line-height: 14.36px;
+        box-shadow: -1px 1px black;
+      }
+    }
+
+    .buy_form {
+      h1 {
+        font-size: 24px;
+        text-shadow: -0.96px 0.96px black;
+      }
+
+      .form-container {
+        flex-direction: column;
+        gap: 1.3em;
+        margin-top: 6em;
+        /* height: 323.15px; */
+
+        .form2 {
+          width: 347.29px;
+          padding: 24px;
+          /* background-image: url(${BgImage}); */
+          border: 2.01px solid black;
+          box-shadow: -2px 2px black;
+          border-radius: 16.07px;
+          gap: 24px;
+
+          .item {
+            display: flex;
+            justify-content: space-between;
+            width: 100%;
+            height: 35px;
+            gap: 16px;
+            border-bottom: 2px solid black;
+            span {
+              font-size: 10.71px;
+              line-height: 12.82px;
+              letter-spacing: 1.5px;
+            }
+          }
+        }
+        .form {
+          /* width: 537px; */
+          padding: 24px 16px;
+          /* background-image: url(${BgImage}); */
+          border: 2.01px solid black;
+          box-shadow: -2px 2px black;
+          border-radius: 16.07px;
+          /* gap: 21.43em; */
+
+          button {
+            background: #ffda88;
+            padding: 10.71px 21.43px;
+            border-radius: 16px;
+            border: 2.01px solid black;
+            font-size: 16.07px;
+            line-height: 19.25px;
+            letter-spacing: 1.5px;
+            box-shadow: -2px 2px black;
+          }
+
+          .form-item {
+            padding: 16px;
+            gap: 21.43px;
+            border: 2.01px solid black;
+            border-radius: 16.07px;
+            label {
+              font-size: 10.71px;
+              line-height: 12.82px;
+              letter-spacing: 1.5px;
+            }
+
+            .input-wrapper {
+              height: 34.43px;
+              input {
+                font-size: 10.71px;
+                letter-spacing: 1.5px;
+                line-height: 12.82px;
+                height: 100%;
+                width: 70%;
+              }
+
+              .input-txt {
+                padding: 10.71px;
+                border: 2.01px solid black;
+                border-radius: 5.36px;
+                font-size: 10.71px;
+                line-height: 12.82px;
+                letter-spacing: 1.5px;
+              }
             }
           }
         }
--- src/pages/Presale.jsx
+++ src/pages/Presale.jsx
@@ -4,64 +4,63 @@ import Wrapper from '../assets/wrappers/Presale'
 const Presale = () => {
   return (
     <Wrapper>
-          <div className='header-container'>
-              <button>Connect Wallet</button>
-        </div>
+      <div className="header-container">
+        <button>Connect Wallet</button>
+      </div>
 
-          <div className='buy_form'>
-              <h1>BUY $ARB</h1>
-              <div className='form-container'>
-                    <form>
-                  <div className='form'>
-                        <div className='form-item'>
-                              <label>From</label>
-                              <div className='input-wrapper'>
-                                <input type='text' placeholder='ENTER ETH' required/>
-                                <div className='input-txt'>ETH</div>
-                              </div>
-                        </div>
-                        <div className='form-item'>
-                              <label>To</label>
-                              <div className='input-wrapper'>
-                                <input type='text' placeholder='ENTER ARB' required/>
-                                <div className='input-txt'>ARB</div>
-                              </div>
-                        </div>
-                  <button>BUY</button>
-                  </div>
-                    </form>
-                    
-                  <div className='form2'>
-                      <div className='item'>
-                          <span>token name</span>
-                          <span>Aerobull</span>
-                       </div>
-                      <div className='item'>
-                          <span>token symbol</span>
-                          <span>$arb</span>
-                       </div>
-                      <div className='item'>
-                          <span>decimal</span>
-                          <span>18</span>
-                       </div>
-                      <div className='item'>
-                          <span>token for eth</span>
-                          <span>250,000</span>
-                       </div>
-                      <div className='item'>
-                          <span>min contribution</span>
-                          <span>0.1 eth</span>
-                       </div>
-                      <div className='item'>
-                          <span>max contribution</span>
-                          <span>0.5 eth</span>
-                       </div>
-                       
-                  </div>
+      <div className="buy_form">
+        <h1>BUY $ARB</h1>
+        <div className="form-container">
+          <div>
+            <form className="form">
+              <div className="form-item">
+                <label>From</label>
+                <div className="input-wrapper">
+                  <input type="text" placeholder="ENTER ETH" required />
+                  <div className="input-txt">ETH</div>
+                </div>
               </div>
+              <div className="form-item">
+                <label>To</label>
+                <div className="input-wrapper">
+                  <input type="text" placeholder="ENTER ARB" required />
+                  <div className="input-txt">ARB</div>
+                </div>
+              </div>
+              <button>BUY</button>
+            </form>
+          </div>
+
+          <div className="form2">
+            <div className="item">
+              <span>token name</span>
+              <span>Aerobull</span>
+            </div>
+            <div className="item">
+              <span>token symbol</span>
+              <span>$arb</span>
+            </div>
+            <div className="item">
+              <span>decimal</span>
+              <span>18</span>
+            </div>
+            <div className="item">
+              <span>token for eth</span>
+              <span>250,000</span>
+            </div>
+            <div className="item">
+              <span>min contribution</span>
+              <span>0.1 eth</span>
+            </div>
+            <div className="item">
+              <span>max contribution</span>
+              <span>0.5 eth</span>
+            </div>
+          </div>
         </div>
+      </div>
     </Wrapper>
-  )
+  );
 }
 
 export default Presale
