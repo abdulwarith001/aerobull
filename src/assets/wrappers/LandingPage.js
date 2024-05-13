4effbf44e9d6c802a08c12ae29fc56173d3ace4c
Abdulwarith <adebayoayomide646@gmail.com>
2024-05-13 09:50:50 +0100

    hero design changed

--- src/assets/wrappers/LandingPage.js
+++ src/assets/wrappers/LandingPage.js
@@ -2,50 +2,7 @@ import styled from 'styled-components'
 import hero_back from '../images/hero_back.png'
 
 const Wrapper = styled.section`
-    z-index: -9999;
-  .milkyguy {
-     margin: -2em 0 0;
-    display: flex;
-    justify-content: space-between;
-    align-items: flex-end;
-    /* background: url(${hero_back});
-    background-position: center;
-    background-size: cover;
-    background-repeat: no-repeat;
-    object-fit: cover; */
-    .mobile {
-      display: none;
-    }
-
-    .desktop {
-      display: block;
-    }
-    h1 {
-      color: white;
-      -webkit-text-stroke: 1px #dbf0ff; /* For WebKit browsers like Chrome and Safari */
-      font-size: 237.39px;
-      line-height: 1;
-      font-weight: 400;
-      font-family: "MilkyNice", sans-serif;
-    }
-  }
-
-  @media (max-width: 992px) {
-    .milkyguy {
-      margin: -12em 0 0;
-
-      .mobile {
-        display: block;
-      }
-
-      .desktop {
-        display: none;
-      }
-      h1 {
-        display: none;
-      }
-    }
-  }
+ 
 `;
 
 export default Wrapper
