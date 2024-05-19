63ce4dd7d6866571bf2481f5c2594b98bf0a2f87
Abdulwarith <adebayoayomide646@gmail.com>
2024-05-19 22:00:14 +0100

    major update test

--- src/main.jsx
+++ src/main.jsx
@@ -7,17 +7,24 @@ import {
   defaultConfig as ethersDefaultConfig,
 } from "@web3modal/ethers/react";
 import { WagmiConfig } from "wagmi";
-import { arbitrum } from "wagmi/chains";
 import { defaultWagmiConfig } from "@web3modal/wagmi/react/config";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query"; // Import QueryClient and QueryClientProvider
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const projectId = "99ba00417662aecb6fb030b897854729";
 
 const metadata = {
   name: "test",
   description: "Web3Modal Example",
-  url: "https://aerobull.netlify.app", // origin must match your domain & subdomain
+  url: "https://aerobull.netlify.app",
   icons: ["https://avatars.githubusercontent.com/u/37784886"],
+};
+
+const baseNetwork = {
+  chainId: 8453,
+  name: "Base",
+  currency: "ETH",
+  explorerUrl: "https://basescan.org",
+  rpcUrl: "https://mainnet.base.org",
 };
 
 const ethersConfig = ethersDefaultConfig({
@@ -25,27 +32,11 @@ const ethersConfig = ethersDefaultConfig({
   enableEIP6963: true,
   enableInjected: true,
   enableCoinbase: true,
-  rpcUrl: "https://cloudflare-eth.com",
-  defaultChainId: 1,
+  rpcUrl: baseNetwork.rpcUrl,
+  defaultChainId: baseNetwork.chainId,
 });
 
-const mainnet = {
-  chainId: 1,
-  name: "Ethereum",
-  currency: "ETH",
-  explorerUrl: "https://etherscan.io",
-  rpcUrl: "https://cloudflare-eth.com",
-};
-
-const sepolia = {
-  chainId: 11155111, // Sepolia chain ID
-  name: "Sepolia",
-  currency: "ETH",
-  explorerUrl: "https://sepolia.etherscan.io",
-  rpcUrl: "https://sepolia.infura.io/v3/99ba00417662aecb6fb030b897854729", // Replace with your Infura Project ID
-};
-
-const chains = [mainnet, arbitrum];
+const chains = [baseNetwork];
 
 const wagmiConfig = defaultWagmiConfig({
   projectId,
@@ -60,7 +51,7 @@ createWeb3Modal({
   enableAnalytics: true,
 });
 
-const queryClient = new QueryClient(); // Create a QueryClient instance
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
